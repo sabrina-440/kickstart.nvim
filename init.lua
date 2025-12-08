4e2426b58b0605563042147cfc04137c5e291f5a
sabrina-440 <sabrinaxjp@gmail.com>
2025-12-07 19:25:49 -0500

    fix opsie

--- init.lua
+++ init.lua
@@ -237,6 +237,9 @@ end
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
 
+
+vim.opt.shiftwidth = 4,
+vim.opt.expandtab = true,
 -- [[ Configure and install plugins ]]
 --
 --  To check the current status of your plugins, run
@@ -251,8 +254,6 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-  vim.opt.shiftwidth = 4,
-  vim.opt.expandtab = true,
   'ThePrimeagen/vim-be-good',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
